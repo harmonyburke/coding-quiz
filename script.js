6433af353c8bd61cc0cb715a960397a7ba5d8438
Harmony Burke <theharmonyburke@gmail.com>
2023-10-11 19:26:40 -0500

    fixed timer issue

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 var questionNumber=0;
+var gameOver=document.querySelector("#gameOver");
 var choices=document.querySelector("#choices");
-var question1=document.querySelector("#question-1");
-var question2=document.querySelector("#question-2");
-var question3=document.querySelector("#question-3");
-var question4=document.querySelector("#question-4");
-var question5=document.querySelector("#question-5");
 // added var with queryselector for each question
 var questionContainer=document.querySelector(".questions");
 var answer1=document.querySelector("#answer-1");
@@ -15,11 +11,11 @@ var answer5=document.querySelector("#answer-5");
 // added var for all answers
 var start=document.querySelector("#start-button");
 // added var for start button
-var timerElement=document.querySelector(".timer");
+var timerElement=document.querySelector("#timer");
 // added var for timer element
 var next=document.querySelector("#next");
-// var for next button
-// var timerCount=60;
+// bank for all questions and answers
+var timerCount=10;
 var questionBank=[
     {
         question:"String values must be enclosed within ____ when being assigned to variables.",
@@ -49,35 +45,43 @@ var questionBank=[
 ];
 
 // using Show to show the questions as the user clicks the button
-function startGame(){
-    // questionContainer.removeAttribute("class");
-    timerCount=60;
-    startTimer();
-    showQuestion()
-}
 // function to append questions to page
 function showQuestion(){
     var currQuestion=questionBank[questionNumber]
-    var questionTitle=document.getElementById("questionHeader")
+    var questionTitle=document.getElementById("questionHeader");
     questionTitle.textContent=currQuestion.question;
     choices.innerHTML="";
-
+    
+    // for (var i=0; i<currQuestion.choices.length;i++){
+    //     var choices=currQuestion.choices[i];
+    //     var choiceBtn=document.createElement("button");
+    //     choiceBtn.setAttribute("class","choice");
+    //     choiceBtn.setAttribute("value", choice);
+    //     choiceBtn.textContent= i + 1 + ". " + choice;
+    //     choices.appendChild(choiceBtn);
+    // }
     // loop through questions and choices
     currQuestion.choices.forEach(function(option,i){
         var choiceIndex=document.createElement("button")
-        // choice index.setAttribute("class","buttonClass")
+       
         choiceIndex.setAttribute("value", option)
-        choiceIndex.textContent=i+1+". "+option;
-        // choiceIndex.onclick=optionChosen;
+        choiceIndex.textContent=i+1+". "+ option;
         choices.appendChild(choiceIndex)
     })
-    console.log('these aremy choices', choices)
+    // console.log('these are my choices', choices)
 }
 // this function will be called if the user does not finish in the alotted time
 function timesUp(){
-    wordBlank.textContent= "Times Up!"
+    gameOver.textContent= "Too slow!"
+    if (timerCount===0){
+        clearInterval(timer);
+    }
 }
-// this function is meant to start the timer and show the questions when the user clicks the start button
+function startGame(){
+    timerCount=10;
+    startTimer();
+    showQuestion()
+}
 start.addEventListener("click", startGame)
 function startTimer(){
     timer=setInterval(function(){
@@ -86,24 +90,10 @@ function startTimer(){
         if (timerCount===0){
             timesUp();
             clearInterval(timer);
+            console.log("timer")
        }
+    //    timer keeps going into the negative
 
     },1000);
 }
-// start.addEventListener("click",function startGame(){
-//     timerElement=setInterval(function(){
-//         timerCount--;
-//         timerElement.textContent=timerCount + "seconds left!"
-//         if (timerCount=0){
-//             TimesUp();
-//         }
-        
-//         // function show(){
-//         //         document.getElementById("ul").style.display="block"; 
-//         // }
-
-//         // show();
-
-//     })
-// }) 
 
